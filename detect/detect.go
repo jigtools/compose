b352b1533f6e13497af389498cb98fea84966155
Sven Dowideit <SvenDowideit@home.org.au>
2017-10-13 17:44:45 +1000

    simplistic detect script / cloud-init
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- detect/detect.go
+++ detect/detect.go
@@ -5,12 +5,16 @@ import (
 	"io/ioutil"
 
 	"gopkg.in/yaml.v2"
+	"strings"
 )
 
 func Type(filename string) (filetype string, err error) {
 	contents, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return "", fmt.Errorf("FAILED to open %s : %s", filename, err)
+	}
+	if t, err := tryScript(contents); err == nil {
+		return t, nil
 	}
 	if t, err := tryYAML(contents); err == nil {
 		return t, nil
@@ -27,4 +31,17 @@ func tryYAML(contents []byte) (filetype string, err error) {
 	}
 
 	return "json", nil
+}
+
+// looks for the leading # (and then at the rest of the file...)
+func tryScript(contents []byte) (filetype string, err error) {
+	str := string(contents[0:100])
+	if strings.HasPrefix(str, "#cloud-config") {
+		// TODO: also test that its a vaild yaml and cloud-init (or even if its rancheros / ubuntu etc
+		return "cloud-config", nil
+	}
+	if strings.HasPrefix(str, "#!") {
+		return "script", nil
+	}
+	return "", fmt.Errorf("FAILED to detect a script file")
 }
